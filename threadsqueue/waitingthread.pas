46c43bec587e301fdee48fb852fc744b913a94af
ozon85 <79962382+ozon85@users.noreply.github.com>
2022-08-31 08:35:42 +0700

    Update waitingthread.pas

--- threadsqueue/waitingthread.pas
+++ threadsqueue/waitingthread.pas
@@ -8,31 +8,64 @@ uses
   Classes{, SysUtils};
 
 type
+TWaitingThread = class;
+TWaitingThreadlist=class(TFPList)
+protected
+    function Get(Index: Integer): TWaitingThread;
+    procedure Put(Index: Integer; Item: TWaitingThread);
+public
+    function Last: TWaitingThread;
+    property Items[Index: Integer]: TWaitingThread read Get write Put; default;
+end;
+
+TNotifyMethod = procedure(Sender: TObject);
 
 TWaitingThread = class(TThread)
   private
     FEvent:PRTLEvent;
     FExecuteMethod: TThreadExecuteCallBack;
     FOnExecEnd : TNotifyCallBack;
-    FData : Pointer;
+    FData: Pointer;
+    FworkDone:boolean;
     function GetWaitingStatus:boolean;
+    procedure CallOnTerminate;
   protected
     procedure Execute; override;
     procedure WaitEvent;
     procedure SetEvent;
     procedure SyncCall;
+    procedure DoTerminate;override;
   public
+    OnTerminateMethod:TNotifyCallBack;
     Constructor Create(CreateSuspended: Boolean=false;const StackSize: SizeUInt = DefaultStackSize;
                                   AFreeOnTerminate:boolean=true);
     destructor Destroy; override;
     function ExecuteInThread(AMethod : TThreadExecuteCallback; AData : Pointer = Nil;
                               AOnExecEnd: TNotifyCallBack = Nil):boolean;
+    procedure WaitForWorkDone;
     procedure EndWork;
     property ReadyToExecute:boolean read GetWaitingStatus;
+    property Data : Pointer read FData write FData;
+    property ExecuteMethod: TThreadExecuteCallBack read FExecuteMethod write FExecuteMethod;
 end;
 
 
 implementation
+
+function TWaitingThreadlist.Get(Index: Integer): TWaitingThread;
+begin
+Result:=TWaitingThread(inherited);
+end;
+
+procedure TWaitingThreadlist.Put(Index: Integer; Item: TWaitingThread);
+begin
+inherited put(Index,Item);
+end;
+
+function TWaitingThreadlist.Last: TWaitingThread;
+begin
+result:=TWaitingThread(inherited);
+end;
 
 
 Constructor TWaitingThread.Create(CreateSuspended: Boolean=false;
@@ -40,6 +73,7 @@ Constructor TWaitingThread.Create(CreateSuspended: Boolean=false;
                                   AFreeOnTerminate:boolean=true);
 begin
 FEvent:=RTLEventCreate;
+RTLEventResetEvent(Fdata);
 FreeOnTerminate:=AFreeOnTerminate;
 FExecuteMethod:=nil;
 FOnExecEnd:=nil;
@@ -56,20 +90,19 @@ end;
 procedure TWaitingThread.Execute;
 begin
 repeat
-  if FExecuteMethod=nil then
-    WaitEvent
-  else begin
+  WaitEvent;//Wait to get or set FExecuteMethod, Fdata
+  if FExecuteMethod<>nil then begin
     FExecuteMethod(Fdata);
-    Synchronize(@SyncCall);
+    Queue(@SyncCall);//Thread will fall down if exception in main thread
+    //Synchronize(TThreadMethod(@SyncCall));//Thread will fall down if exception in main thread
   end;
 until Terminated;
 end;
 
 procedure TWaitingThread.SyncCall;
 begin
-FExecuteMethod:=nil;
 if FOnExecEnd<>nil then
-  FOnExecEnd(self,Fdata);
+  FOnExecEnd(self,{SyncCallData}Fdata);
 end;
 
 procedure TWaitingThread.SetEvent;
@@ -79,12 +112,21 @@ end;
 
 procedure TWaitingThread.WaitEvent;
 begin
+FworkDone:=true;
 RTLEventWaitFor(FEvent);
+RTLEventResetEvent(Fdata);
+FworkDone:=false;
 end;
 
 function TWaitingThread.GetWaitingStatus:boolean;
 begin
 result:=FExecuteMethod=nil;
+end;
+
+procedure TWaitingThread.WaitForWorkDone;
+begin
+while (not FworkDone)do
+ CheckSynchronize(500);
 end;
 
 procedure TWaitingThread.EndWork;
@@ -93,11 +135,22 @@ Terminate;
 SetEvent;
 end;
 
+procedure TWaitingThread.DoTerminate;
+begin
+  Synchronize(@CallOnTerminate);
+end;
+
+procedure TWaitingThread.CallOnTerminate;
+begin
+  if Assigned(OnTerminateMethod)then
+    OnTerminateMethod(Self,Fdata);
+end;
 
 function TWaitingThread.ExecuteInThread(AMethod : TThreadExecuteCallback; AData : Pointer = Nil;
                               AOnExecEnd: TNotifyCallBack = Nil):boolean;
 begin
 result:=ReadyToExecute and (AMethod<>nil);
+if not result then exit;
 FExecuteMethod := AMethod;
 FOnExecEnd := AOnExecEnd;
 FData:=AData;
@@ -105,4 +158,3 @@ SetEvent;
 end;
 
 end.
-
